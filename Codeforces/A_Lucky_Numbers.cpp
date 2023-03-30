aea6cc634dffe3b13e453f02c3cce825f2800b88
Zahid031 <arnabrahmanzahid03@gmail.com>
2023-03-30 21:50:00 +0600

    Modified

--- Codeforces/A_Lucky_Numbers.cpp
+++ Codeforces/A_Lucky_Numbers.cpp
@@ -1,47 +1,41 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-int get_luckiness(int num)
+int findLargestSmallest(int num)
 {
-    int max_digit = 0, min_digit = 9;
-    while (num > 0)
-    {
-        int digit = num % 10;
-        max_digit = max(max_digit, digit);
-        min_digit = min(min_digit, digit);
-        num /= 10;
-    }
-    return max_digit - min_digit;
-}
+    int largestDigit = 0;
+    int smallestDigit = 9;
+    int digit;
+    while (num)
 
+    {
+        digit = num % 10;
+        largestDigit = max(digit, largestDigit);
+
+        smallestDigit = min(digit, smallestDigit);
+        num = num / 10;
+    }
+    return (largestDigit - smallestDigit);
+}
 int main()
 {
     int t;
     cin >> t;
     while (t--)
     {
-        int l, r;
-        cin >> l >> r;
-        int max_luckiness = -1, luckiest_num = -1;
-        for (int num = max(l, 1000000 - 8); num <= r; num++)
+        int a, b;
+        int max, ans = 0, out = 0;
+        ;
+        cin >> a >> b;
+        for (int i = a; i <= b; i++)
         {
-            int luckiness = get_luckiness(num);
-            if (luckiness >= 8)
+            max = findLargestSmallest(i);
+            if (max > ans)
             {
-                max_luckiness = luckiness;
-                luckiest_num = num;
-                break;
+                ans = max;
+                out = i;
             }
         }
-        if (luckiest_num == -1)
-        {
-            cout << "No lucky number found in the range." << endl;
-        }
-        else
-        {
-            cout << luckiest_num << endl;
-        }
+        cout << out << endl;
     }
-    return 0;
 }
